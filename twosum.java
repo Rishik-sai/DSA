9426acb15757e7bab966c39218dea863a87e5db4
Rishik-sai <istylesai@gmail.com>
2025-10-02 13:37:20 +0530

    day 11

---
+++ twosum.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+public class twosum {
+    public static void main(String[] args) {
+        int[] nums = {2,6,5,8,11};
+        int target = 14;
+        int[] result = twoSum(nums, target);
+        if (result != null) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two sum solution found.");
+        }
+    }
+
+    static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
