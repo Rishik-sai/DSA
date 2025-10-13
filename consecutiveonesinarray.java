10d1041ab0266abc0f77c13cff294cdf150be41d
Rishik-sai <istylesai@gmail.com>
2025-10-13 15:43:58 +0530

    day 22

---
+++ consecutiveonesinarray.java
@@ -0,0 +1,16 @@
+public class consecutiveonesinarray {
+    public static void main(String[] args) {
+        int arr[]={1,0,1,1,1};
+        int count=0,maxcount=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==1){
+                count=count+1;
+            }else{
+                maxcount = Math.max(maxcount, count);
+                count=0;
+            }
+        }
+        maxcount = Math.max(maxcount, count);
+        System.out.println(maxcount);
+    }
+}
